733ab63ab2aba411cc7237723b04fded50957017
Fernando Sanchez <fernandosanchez1128@gmail.com>
2016-06-27 23:12:23 -0500

    Create MaxAccess.java
    El servidor mas accedido

---
+++ MaxAccess.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MaxAccess {
+  private  static int max_acount=0;	
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text server = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      server.set(itr.nextToken());
+      context.write(server, one);
+    }
+  }
+
+ public static class MaxReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+    Text server =new Text();
+    
+    public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {  sum += val.get();      }
+
+      if(sum>max_acount)
+      {
+		max_acount=sum;
+		server.set(key);
+      }
+    }
+
+    protected void cleanup(Context context) throws IOException, InterruptedException{
+    context.write(server, new IntWritable(max_acount));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(MaxAccess.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(MaxReducer.class);
+    job.setReducerClass(MaxReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
